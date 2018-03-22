a329d6d6cb55e5186c72771592413543280368cf
byash22 <34084194+byash22@users.noreply.github.com>
2018-03-22 22:59:27 +0530

    Update CreatingExcelFile.java

--- CreatingExcelFile.java
+++ CreatingExcelFile.java
@@ -4,10 +4,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Iterator;
-
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
@@ -49,34 +45,7 @@ public class CreatingExcelFile {
            public static void copySheet(XSSFSheet inputSheet,XSSFSheet outputSheet) 
            { 
               int rows=inputSheet.getLastRowNum(); 
-              System.out.println("There are " + rows+" rows in inputsheet "+inputSheet.getSheetName()); 
-               
-                int currentRowIndex=0; if(rows>0)
-		{
-			Iterator<Row> rowIterator=inputSheet.iterator();
-			while(rowIterator.hasNext())
-			{
-				int currentCellIndex=0;
-				Iterator<Cell> cellIterator=rowIterator.next().cellIterator();
-				while(cellIterator.hasNext())
-				{
-					  
-					String cellData=cellIterator.next().toString();
-					
-					if(currentCellIndex==0)
-					    outputSheet.createRow(currentRowIndex).createCell(currentCellIndex).setCellValue(cellData);
-					
-					else
-						
-						outputSheet.getRow(currentRowIndex).createCell(currentCellIndex).setCellValue(cellData);
-					
-					currentCellIndex++;
-				}
-				currentRowIndex++;
-			}
-			System.out.println((currentRowIndex-1)+" rows added to outputsheet "+outputSheet.getSheetName());
-			System.out.println();
+              System.out.println("\nThere are " + rows+" rows in inputsheet "+inputSheet.getSheetName()); 
 		
 		    }    
         }
-}
